90eeaa4b79f1c1fc5bf148c96802b66e5e2d4ed2
Pratmika Shridhar Nayak <pratmikashridharnayak@Pratmikas-Mac-mini.local>
2017-09-04 09:53:31 +0530

    Initial Commit

---
+++ CollegeStudentData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollegeStudentData
+//
+//  Created by Pratmika Shridhar Nayak on 04/09/17.
+//  Copyright © 2017 Pratmika Shridhar Nayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
